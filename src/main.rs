8dea5b3f267b9e1c81309e78a71a9f2b7584f0e1
Gunnar Klämke <gunnar-klaemke@gmx.de>
2018-03-30 20:11:48 +0200

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+#[macro_use]
+extern crate log;
+extern crate simple_logging;
+
+use std::io::{self, Write};
+
+#[derive(Debug)]
+enum Command {
+    Quit,
+    Unknown,
+    Uci,
+}
+
+impl Command {
+    fn from_str(command: &str) -> Command{
+        match command.trim(){
+            "quit" => Command::Quit,
+            "uci" => Command::Uci,
+            other => {
+                warn!("Unkown Command: {}", other);
+                Command::Unknown
+            }
+        }
+    }
+
+    fn is_quit(&self) -> bool{
+        match *self {
+            Command::Quit => true,
+            _ => false
+        }
+    }
+
+    fn answer(&self) -> &'static str{
+        match *self{
+            Command::Quit => "",
+            Command::Unknown => "",
+            Command::Uci => "id name Luci\nid author Gunnar Klämke, Markus Klein\nuciok\n"
+        }
+    }
+}
+
+fn main() {
+    simple_logging::log_to_file("luci.log", log::LevelFilter::Info).unwrap();
+    info!("Start");
+    let mut command = String::new();
+    loop {
+        command.clear();
+        match io::stdin().read_line(&mut command){
+            Ok(_size) => {
+                let command = Command::from_str(&command);
+                debug!("{:?}", command);
+                if command.is_quit(){
+                    break;
+                }
+               
+                io::stdout().write(command.answer().as_bytes()).unwrap();
+                io::stdout().flush().unwrap();             
+            },
+            Err(e) => { 
+                error!("Error reading command:{}", e);
+                break; 
+            }
+        }
+    }
+
+}
